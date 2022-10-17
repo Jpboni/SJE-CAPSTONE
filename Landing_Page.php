4fedd85752f175c974a28eddc8f7b4f6bb4a0158
Jpboni <108069413+Jpboni@users.noreply.github.com>
2022-10-17 20:12:05 +0800

    Add files via upload

---
+++ Landing_Page.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+
+<title>Get Started</title>
+<link rel="stylesheet" href="Directories/css/main.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+
+<div class="txt">
+<div class="font-face">
+
+<h1 class ="headtitle">"EDUKASYONG ABOT KAMAY" </h1>
+<p class ="headtitle">-Kahit saan, Kahit Kailan.
+  SIMULAN ANG PAGAARAL NGAYON-</p> </div>
+
+<!--Start ng log in button-->
+
+</div>
+
+
+
+
+
+<div class="navbar">
+<div class="logo">
+    SAN JUAN SCHOOL ELMS
+
+</div>
+
+
+<a class="btn">
+<span></span>
+<span></span>
+<span></span>
+</a>
+
+
+<div class="menu">
+
+   <a href="Get_started.php">get started</a>
+   <a href="#link">about</a>
+   <a href="LOGIN PAGE/Login(2)/Login_Page.php">Log in</a>
+
+</div>
+</div>
+<div class="header">
+
+</div>
+
+
+</head>
+<body>
+
+
+
+
+
+<div class="content">
+<a id="link">
+<p>
+<hr></a>
+<br><br>
+</p>
+<p>
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+ <br><br><br>
+                  </p>
+
+</div>
+<hr>
+
+
+
+
+<!-- FOOTER -->
+<div class="w-full py-1 px-2 clearfix" style="border-top: 1px solid rgba(0,0,0,0.12);">
+
+        <span class="float-left">
+        Contact Us
+        <a href="#" class="styled-link">Here</a>
+        </span>
+        <span class="float-right">
+        sd <a href="#" class="styled-link">Capstones</a>
+        </span>
+
+</div>
+
+
+
+<script type="text/javascript">
+
+$(".btn").on("click",function(){
+  $('.menu').toggleClass("show");
+});
+
+
+</script>
+
+
+</body>
+</html>
